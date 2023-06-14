2232f08fa4f3b60a354ae74ada986e17b2fe0ccf
raffay <0.abdulraffay@gmail.com>
2023-06-14 15:36:24 +0500

    Almost Complete

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+    sliders()
+    navBar()
+    menuOpen()
+})
+
+
+function sliders() {
+    $('.slider').slick({
+        centerMode: true,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 778,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+
+    $('.toppingSlider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: false,
+        dots: true,
+    })
+}
+
+
+function navBar() {
+    const toggle = $('#menu-toggle')
+    const cross = $('#crossNav')
+    const navbar = $('.mobile-nav')
+    var nav = false;
+
+
+    cross.click(function () {
+        navbar.fadeOut('fast')
+        nav = false;
+    })
+
+    toggle.click(function () {
+        if (nav == false) {
+            $('.mobile-nav').fadeIn('fast');
+            nav = true
+        }
+        else {
+            $('.mobile-nav').fadeOut('fast');
+            nav = false
+        }
+
+    })
+}
+
+function menuOpen(){
+    const menuBtn = $('.openMenu')
+    const menuList = $('.menuList')
+    let isOpen = false;
+
+    menuBtn.click(function(){
+        if(isOpen){
+            menuList.slideUp()
+            isOpen = false;
+        }
+        else{
+            menuList.slideDown()
+            isOpen = true;
+        }
+    })
+}
